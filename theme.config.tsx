16a6da58f6b0455a4b08f6e54e061628aa45b944
vedantraval2310 <76436755+vedantraval2310@users.noreply.github.com>
2024-04-18 08:51:20 +0530

    init

---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>VWS Documentations</span>,
+
+  footer: {
+    text: 'VWS',
+  },
+}
+
+export default config
